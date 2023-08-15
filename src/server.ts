6d52c7d7e9dbdb8cf87f61810fa3bf7d9aa9a2aa
Ivan Passos <ivanpassosfilho@gmail.com>
2023-08-15 20:59:34 -0300

    Aula 7 - Realizando queries com Knex

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,33 @@
 import fastify from 'fastify';
 import { knex } from './database';
 
+import crypto from 'node:crypto';
+
 const app = fastify();
 
-app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*');
+app.get('/transactions', async () => {
+  // const transactions = await knex('transactions')
+  //   .insert({
+  //     id: crypto.randomUUID(),
+  //     title: 'Transação de teste',
+  //     amount: 1200,
+  //   })
+  //   .returning('*');
 
-  return tables;
+  const transactions = await knex('transactions').select(
+    '*',
+  );
+
+  return transactions;
+});
+
+app.get('/sessions', async () => {
+  const transactions = await knex('transactions').insert({
+    id: crypto.randomUUID(),
+    tittle: 'Transação de teste',
+  });
+
+  return transactions;
 });
 
 app
