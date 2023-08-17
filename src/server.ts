c10e72ffd5fa8af5892e03ff630d2dfe4a5b262c
Ivan Passos <ivanpassosfilho@gmail.com>
2023-08-17 00:44:18 -0300

    Aula 12 - Criação de transações

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
 import fastify from 'fastify';
-import { knex } from './database';
-
-import crypto from 'node:crypto';
 import { env } from './env';
 import { transactionsRoutes } from './routes/transactions';
 
 const app = fastify();
 
-app.register(transactionsRoutes);
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
+});
 
 app
   .listen({
