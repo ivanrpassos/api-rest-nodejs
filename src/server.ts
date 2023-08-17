04a8929030a025269702a96b7eba68a081edf992
Ivan Passos <ivanpassosfilho@gmail.com>
2023-08-17 00:32:44 -0300

    Aula 11 - Plugins do Fastify

--- src/server.ts
+++ src/server.ts
@@ -3,33 +3,11 @@ import { knex } from './database';
 
 import crypto from 'node:crypto';
 import { env } from './env';
+import { transactionsRoutes } from './routes/transactions';
 
 const app = fastify();
 
-app.get('/transactions', async () => {
-  // const transactions = await knex('transactions')
-  //   .insert({
-  //     id: crypto.randomUUID(),
-  //     title: 'Transação de teste',
-  //     amount: 1200,
-  //   })
-  //   .returning('*');
-
-  const transactions = await knex('transactions').select(
-    '*',
-  );
-
-  return transactions;
-});
-
-app.get('/sessions', async () => {
-  const transactions = await knex('transactions').insert({
-    id: crypto.randomUUID(),
-    tittle: 'Transação de teste',
-  });
-
-  return transactions;
-});
+app.register(transactionsRoutes);
 
 app
   .listen({
