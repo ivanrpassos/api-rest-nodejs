dd7dd83aa38c2156d3223b7f83555f3945bfff51
Ivan Passos <ivanpassosfilho@gmail.com>
2023-08-18 18:14:42 -0300

    Aula 14 - Listagem das transações

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -6,6 +6,30 @@ import { randomUUID } from 'crypto';
 export async function transactionsRoutes(
   app: FastifyInstance,
 ) {
+  app.get('/', async (req, res) => {
+    const transactions = await knex(
+      'transactions',
+    ).select();
+
+    return { transactions };
+  });
+
+  app.get('/:id', async (req, res) => {
+    const getTransactionsParamsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = getTransactionsParamsSchema.parse(
+      req.params,
+    );
+
+    const transaction = await knex('transactions')
+      .where('id', id)
+      .first();
+
+    return { transaction };
+  });
+
   app.post('/', async (req, res) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
