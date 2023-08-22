21ac1d8dc45889a19dd564c9ec5ca10f1862d185
Ivan Passos <ivanpassosfilho@gmail.com>
2023-08-21 21:49:48 -0300

    Aula 16 - Utilizando cookies no Fastify

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -48,10 +48,22 @@ export async function transactionsRoutes(
     const { title, amount, type } =
       createTransactionBodySchema.parse(req.body);
 
+    let sessionId = req.cookies.sessionId;
+
+    if (!sessionId) {
+      sessionId = randomUUID();
+
+      res.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      });
+    }
+
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     });
 
     return res.status(201).send();
